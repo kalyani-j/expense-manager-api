58aa1ed94f359ce98ea12124c2c3b0654eba515f
kalyani joshi <kalyani121@gmail.com>
2024-09-21 15:59:32 +0530

    added model for expense

---
+++ models/expense.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const expenseSchema = new Schema(
+  {
+    expense_name: {
+      type: String,
+    },
+    expense_category: {
+      type: String,
+    },
+    
+        currency_type: {
+            type: String,
+        },
+    expense_amount: {
+        type: Number,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Expense = mongoose.model('expense', expenseSchema);
+
+module.exports = Expense;
